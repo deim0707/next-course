154a154bf1b0c0079714fa690ece009dae78ded8
Dmitry <DSParshin@beeline.ru>
2021-11-01 21:58:45 +0300

    добавил эмуляция бека с json-server добавил и настроил библиотеку позволяющую запускать несколько нпм-команд - concurrently сделали лоадер, пока загружается пост (сомнительным образом)

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,14 +1,57 @@
 // задавая имя файла с помощью квадратных скобок. когда файл в папке, мы можем делать динамический рендеринг
 // например /post/[id].js
-
+import {useState, useEffect} from "react";
 import {useRouter} from "next/router";
+import Links from "next/link";
+import {Main} from "../../components-layouts/MainLayout";
 
-export default function Post() {
+// сомнительно два раза делать одно и тоже. ради чего создали эту функцию
+const getData = async (id) => {
+    const response = await fetch(`http://localhost:4200/posts/${id}`);
+    return await response.json();
+}
+
+// очень сомнительное решение, добавлять столько логики для лоадинга на загрузку. поискать решения лучше
+export default function Post({post: postFromBack}) {
     const router = useRouter();
-    const {id} = router.query; // достали переданный параметр
+    const {id} = router.query; // достали переданный параметр из адресной строки
+
+    const [post, setPost] = useState(postFromBack);
+    useEffect(() => {
+        if (!postFromBack) {
+            setPost(getData(id))
+        }
+    }, [])
+
+    if (!post) return <Main><p>Загрузка...</p></Main>
+
+    const {title, body} = post;
     return (
         <div>
-            <div>post id = {id}</div>
+            <h2>{title}</h2>
+            <p>id: {id}</p>
+            <hr/>
+            <p>{body}</p>
+            <hr/>
+            <Links href="/posts"><a>Перейти на все посты</a></Links>
         </div>
     )
+}
+
+// Post.getInitialProps = async (context) => {
+//     const {query, req} = context;
+//     if (!req) return {post: null};
+//     // контекст - https://nextjs.org/docs/api-reference/data-fetching/getInitialProps#context-object
+//     const {id} = query;
+//     const post = await getData(id);
+//     return {post}
+// }
+// современный способ запрашивать данные. функция никогда не вызовется на клиентской части, только на сервере
+export async function getServerSideProps(context) {
+    const {query, req} = context;
+    if (!req) return {post: null};
+    // контекст - https://nextjs.org/docs/api-reference/data-fetching/getInitialProps#context-object
+    const {id} = query;
+    const post = await getData(id);
+    return {props: {post}}
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,42 @@
 import {Main} from "../../components-layouts/MainLayout";
+import Link from "next/link";
+// import {useEffect, useState} from "react";
 
-export default function Index() {
-    return <Main>
-        <h5>posts</h5>
-    </Main>
+export default function Index({posts}) {
+    // так мы обычно запрашиваем данные в реакте
+    // const [posts, setPosts] = useState([]);
+    // const loadPostsOnMount = () => {
+    //     fetch('http://localhost:4200/posts')
+    //         .then(r => r.json())
+    //         .then(r => setPosts(r))
+    // }
+    // useEffect(loadPostsOnMount, [])
+
+    return (
+        <Main>
+            <h5>posts</h5>
+            <ul>
+                {posts.map(post => {
+                    const {id, title, body} = post;
+                    return (
+                        <li key={id}>
+                            {/*КОГДА ЛИНК ВЕДЁТ НА ДИНАМИЧЕСКУЮ СТРАНИЦУ, используем синтаксис Линк ниже*/}
+                            <Link href='/post/[id]' as={`/post/${id}`}>
+                                <a>{title}</a>
+                            </Link>:
+                            {body}
+                        </li>
+                    )
+                })}
+            </ul>
+        </Main>
+    )
+}
+
+// устаревший способ1 запросить данные. чтобы роботы видели хтмл уже с подставленными даными
+// https://nextjs.org/docs/api-reference/data-fetching/getInitialProps
+Index.getInitialProps = async () => {
+    const response = await fetch('http://localhost:4200/posts');
+    const posts = await response.json();
+    return {posts}
 }
